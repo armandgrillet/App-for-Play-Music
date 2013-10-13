5e6f3b18f61f4f8ce93cbb3462b1a592f51da417
Armand Grillet <armand.grillet@gmail.com>
2013-10-13 10:40:27 +0200

    Version 1.0.6

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-	chrome.app.window.create('player.html', {
-	    bounds: {
-	      width: Math.round(screen.availWidth*0.85),
-	      height: Math.round(screen.availHeight*0.85)
-	    }, minWidth:940, minHeight: 340
-	});
-});
+chrome.app.runtime.onLaunched.addListener(
+	function() {
+		chrome.storage.local.get( 
+			["left", "top", "width", "height"], 
+			function(afpm) {
+				if (afpm.left != undefined && afpm.top != undefined && afpm.width != undefined && afpm.height != undefined) {
+					chrome.app.window.create(
+						'player.html', 
+						{
+						    bounds: {
+						    	left: afpm.left,
+						    	top: afpm.top,
+						      	width: afpm.width,
+						      	height: afpm.height
+						    }, 
+						    minWidth:940, minHeight: 340
+						}
+					)
+				}
+				else {
+					chrome.app.window.create(
+						'player.html', 
+						{
+						    bounds: {
+						      	width: Math.round(screen.availWidth*0.85),
+						      	height: Math.round(screen.availHeight*0.85)
+						    }, 
+					    	minWidth:940, minHeight: 340
+						}
+					);
+				}
+			}
+		)
+	}
+);
